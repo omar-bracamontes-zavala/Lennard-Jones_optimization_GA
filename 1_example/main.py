3f16ca1e8245e214a7677fc8b14a09c059e13c9e
Omar A. Bracamontes Zavala <omarbracamontes99@gmail.com>
2021-02-08 23:40:13 -0600

    Se hizo más flexible el criterio de terminación

--- 1_example/main.py
+++ 1_example/main.py
@@ -9,32 +9,43 @@ import errorfunctions as errf
 logger = logging.getLogger(__name__)
 
 def ask_error_function():
-    error_function = input('Enter ErrorFunction (sphere/rosenbrock/ackley/rastrigin/schwefel): ')
+    error_function = int(input('Enter ErrorFunction \n0: sphere\n1: translated sphere\n2: rosenbrock\n3: ackley\n4: rastrigin\n5: schwefel\n: '))
     # Sphere x* = (0,0,...,0)
-    if error_function == 'sphere':
+    if error_function == 0:
         lower_bound = -5.12 # Domain of the target function
         upper_bound = 5.12 # Domain of the target function
-        return errf.sphere, lower_bound, upper_bound, error_function
+        return errf.sphere, lower_bound, upper_bound, 'Sphere'
+    # Translated Sphere x* = (2,2,...,2)
+    elif error_function == 1:
+        lower_bound = -3.12 # Domain of the target function
+        upper_bound = 7.12 # Domain of the target function
+        return errf.trans_sphere, lower_bound, upper_bound, 'Translated Sphere'
     # Rosenbrock x* = (1,1,...,1)
-    elif error_function == 'rosenbrock':
+    elif error_function == 2:
         lower_bound = -2.048 # Domain of the target function
         upper_bound = 2.048 # Domain of the target function
-        return errf.rosenbrock, lower_bound, upper_bound, error_function
+        return errf.rosenbrock, lower_bound, upper_bound, 'Rosenbrock'
     # Ackley x* = (0,0,...,0)
-    elif error_function == 'ackley':
+    elif error_function == 3:
         lower_bound = -32.768 # Domain of the target function
         upper_bound = 32.768 # Domain of the target function
-        return errf.ackley, lower_bound, upper_bound, error_function
+        return errf.ackley, lower_bound, upper_bound, 'Ackley'
     # Rastrigin x* = (0,0,...,0)
-    elif error_function == 'rastrigin':
+    elif error_function == 4:
         lower_bound = -5.12 # Domain of the target function
         upper_bound = 5.12 # Domain of the target function
-        return errf.rastrigin, lower_bound, upper_bound, error_function
+        return errf.rastrigin, lower_bound, upper_bound, 'Rastrigin'
     # Schwefel x* = (420.9687,420.9687,...,420.9687)
-    elif error_function == 'schwefel':
+    elif error_function == 5:
         lower_bound = -500. # Domain of the target function
         upper_bound = 500. # Domain of the target function
-        return errf.schwefel, lower_bound, upper_bound, error_function
+        return errf.schwefel, lower_bound, upper_bound, 'Schwefel'
+    # Invalid error function
+    else:
+        logger.info('Invalid ErrorFunction')
+
+def ask_dimention():
+    return int(input('En que dimension?: '))
 
 def create_individual(lower_bound, upper_bound):
     '''
@@ -84,12 +95,20 @@ def termination_criteria(errors, epsilon, iteration, itermax):
     RETURNS.
     boolean
     '''
-    if abs(np.max(errors) - np.min(errors)) <= epsilon:
-        logger.info('\nTerminationCriteria: delta<=epsilon\n')
+    #if abs(np.max(errors) - np.min(errors)) <= epsilon:
+    #    logger.info('\nTerminationCriteria: delta<=epsilon\n')
+    #    return True
+    if abs(np.min(errors)) == 0.:
+        logger.info('\nTerminationCriteria: MinimumReached')
+        return True
+    elif abs(np.min(errors)) <= epsilon:
+        logger.info('\nTerminationCriteria: f(x*)<=epsilon')
         return True
     elif iteration >= itermax:
         logger.info('\nTerminationCriteria: iteration>=itermax\n')
         return True
+    else:
+        return False
 
 
 def crossover(evaluated_population, cutoff_point):
@@ -166,24 +185,27 @@ def local_technique(population):
 
     if evaluated_foreing_indiv[0][0] <= evaluated_worst_indiv[0][0]:
         population[-1] = foreing_individual
-        logger.info('\nLocalTechniqueApplied\n')
+        #logger.info('\nLocalTechniqueApplied\n')
 
     return population
 
-def plot_errors(generations, means, bests, save=False):
+def plot_errors(generations, means, bests, worsts, save=False):
     fig, ax = plt.subplots()
-    fig.suptitle(f'Optimizing {str_error_function} in R^{n_dim} with GA')
+    fig.suptitle(f'Optimizing {str_error_function} in R^{n_dim} with GA\nBestError: {bests[-1]}')
 
-    ax.plot(generations, means, label='Mean Error')
-    ax.plot(generations, bests,'g', label="Elite Error")
+    ax.plot(generations, means,'b', alpha=0.8, label='Mean Error')
+    ax.plot(generations, bests,'g', alpha=1.0, label="Elite Error")
+    #ax.plot(generations, worsts,'r', alpha=0.6, label="Dreg Error")
 
+    ax.grid(alpha=0.5)
+    ax.set_ylim(ymin=-5)
     ax.set_ylabel('Error')
     ax.set_xlabel('Generation')
     ax.legend()
 
     if save==True:
-        now = datetime.datetime.now().strftime('%Y_%m_%d')
-        plt.savefig(f'{str_error_function}{n_dim}_{now}.png')
+        #now = datetime.datetime.now().strftime('%Y_%m_%d')
+        plt.savefig(f'{str_error_function}_d{n_dim}.png')
 
     plt.show()
 
@@ -217,7 +239,7 @@ def main():
         bests_individual_error_per_generation.append( np.min(population_errors) )
         worsts_individual_error_per_generation.append( np.max(population_errors) )
 
-        logger.info(f'\n\t\t\t Generation: {generation}\t| BestError: {bests_individual_error_per_generation[generation]}\t| WorstError: {worsts_individual_error_per_generation[generation]}\t| Delta: {worsts_individual_error_per_generation[generation]-bests_individual_error_per_generation[generation]}\n')
+        #logger.info(f'\n\t\t\t Generation: {generation}\t| BestError: {bests_individual_error_per_generation[generation]}\t| WorstError: {worsts_individual_error_per_generation[generation]}\t| Delta: {worsts_individual_error_per_generation[generation]-bests_individual_error_per_generation[generation]}\n')
         # Termination criteria
         finished = termination_criteria(population_errors, epsilon, generation, itermax)
         # Crossover
@@ -226,29 +248,30 @@ def main():
         childs = mutation(childs, p_1, p_2)
         # Selection/Replacement
         population = selection_replacement(evaluated_population, childs)
-        # Local Technique
+        # Local Technique (optional)
         population = local_technique(population)
         # New Generation
         generations.append(generation)
         generation += 1
 
-    logger.info(f'\nElite:\n{bests_individual_per_generation[-1]}')
-    # Plots
-    plot_errors(generations, mean_individual_error_per_generation, bests_individual_error_per_generation, save=True)
+    print(f'\n----\t{str_error_function}: dim: {n_dim}\t----')
+    print(f'\nDimension: {n_dim}\t | Generation: {generations[-1]}\nEliteError: {bests_individual_error_per_generation[-1]}\nElite:\n{bests_individual_per_generation[-1]}\n')
+
+    plot_errors(generations, mean_individual_error_per_generation, bests_individual_error_per_generation, worsts_individual_error_per_generation, save=save)
 
 if __name__ == '__main__':
 
     errf, lower_bound, upper_bound, str_error_function = ask_error_function()
     n_dims = [10,20,50] # Dimension of the solutions
 
+    p_1 = 5. # Chance of an individual to be mutated (5.)
+    p_2 = 10. # Cance of a gen to be mutated (10.)
+    itermax = 300
+    save = True
+
     for n_dim in n_dims:
         N_individuals = 20 * n_dim # Population size
-        M_gens = 17 * n_dim # Individual size
-
-        p_1 = 5. # Chance of an individual to be mutated
-        p_2 = 10. # Cance of a gen to be mutated
-
-        epsilon = M_gens/N_individuals
-        itermax = 500
+        M_gens = n_dim #17 * n_dim # Individual size
+        epsilon = M_gens/N_individuals # For termination criteria
 
         main()
